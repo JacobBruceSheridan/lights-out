f71df9dcaa71c48adbc476c295414df880ef7930
JacobBruceSheridan <brucejac@sheridancollege.ca>
2020-03-26 09:01:33 -0400

    Updates

---
+++ precache-manifest.9d38933fedfe5057c5d28927d1d47194.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "dc261e41ffbd94f8ee57",
+    "url": "/lights-out/static/js/main.dc261e41.chunk.js"
+  },
+  {
+    "revision": "45ea534042495dbb2393",
+    "url": "/lights-out/static/js/1.45ea5340.chunk.js"
+  },
+  {
+    "revision": "dc261e41ffbd94f8ee57",
+    "url": "/lights-out/static/css/main.d822ba27.chunk.css"
+  },
+  {
+    "revision": "29cf335dc5833f0392b912a2937a7ed5",
+    "url": "/lights-out/index.html"
+  }
+];
